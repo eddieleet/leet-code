eee0b1fd7138b69c76ddb9d86c138bc00e6f5dd4
eddieleet <eddieleet2@gmail.com>
2023-08-26 16:36:49 +0200

    first-commit

---
+++ two-sum-so1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class   Solutiion{
+public:
+   std:vector<int> twoSum(std::vector<int>& nums, int target){
+    std::undordered_map<int, int> numToIndex; // Map to store number-to-index mapping
+
+ for(int i = 0; i < nums.size(); ++i){
+    int complement = target - nums[i];
+
+    // check if the complement is already in the map
+    if(numToIndex.find(complement) != numToIndex.end()){
+        return{numToIndex[complement], i}
+    }
+    
+    // add the current number and its index to the map
+    numToIndex[nums[i]]=i;
+ }
+
+ return {}; // Return an empty vector if no solution is found
+   }
+
+} ;
+
+ 
+ int main(){
+    // test the solution class
+    return 0;
+ }
+
+
